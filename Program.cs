6dc50aa5f60ccfad8868b11941eece5e0a3c3352
Antonio Enríquez <antonio.enralv@gmail.com>
2021-06-05 13:25:48 -0500

    Ejemplo de lista, agregar datos y lectura

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DPRN2_U3_A4_ANEA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+            //Definimos una lista que guardará datos de tipo String
+            List<string> lista = new List<string>();
+
+            //Se guardan 4 nombre de personas.
+            lista.Add("Antonio");
+            lista.Add("Josimar");
+            lista.Add("Enríquez");
+            lista.Add("Alvarado");
+
+            //Se muestra el contenido de la lista con un ciclo foreach
+            System.Console.WriteLine("Lista de Elementos: ");
+            foreach (string nombre in lista)
+            {
+                System.Console.WriteLine(nombre);
+            }
+            
+        }
+    }
+}
